589ef7e46885d45e86ddb72e5ac2dd3406640a3e
Lucas L <hi@lucas.codes>
2017-06-17 19:51:55 -0400

    added ability to move the list of item up or down

--- imports/ui/pages/ListPage.jsx
+++ imports/ui/pages/ListPage.jsx
@@ -9,14 +9,63 @@ import Message from '../components/Message.jsx';
 export default class ListPage extends BaseComponent {
   constructor(props) {
     super(props);
-    this.state = Object.assign(this.state, { editingTodo: null });
+    this.state = Object.assign(
+      this.state, {
+        editingTodo: null,
+        sortedList: [],
+      });
     this.onEditingChange = this.onEditingChange.bind(this);
+    this.onMoveItem = this.onMoveItem.bind(this);
   }
 
   onEditingChange(id, editing) {
     this.setState({
       editingTodo: editing ? id : null,
     });
+  }
+
+  onMoveItem(e, todo, direction) {
+    e.preventDefault();
+
+    const { todos } = this.props;
+
+    const index = todos.indexOf(todo);
+    const todosLength = todos.length;
+
+    switch (direction) {
+      case 'up': {
+        todos.splice(index, 1);
+
+        let newIndex = index - 1;
+        if (newIndex < 0) {
+          newIndex = todosLength;
+        }
+
+        todos.splice(newIndex - 1, 0, todo);
+        this.setState({
+          sortedList: todos,
+        });
+
+        return;
+      }
+      case 'down': {
+        todos.splice(index, 1);
+
+        let newIndex = index + 1;
+        if (newIndex >= todosLength) {
+          newIndex = 0;
+        }
+
+        todos.splice(newIndex, 0, todo);
+        this.setState({
+          sortedList: todos,
+        });
+        return;
+      }
+
+      default:
+        break;
+    }
   }
 
   render() {
@@ -36,19 +85,26 @@ export default class ListPage extends BaseComponent {
         />
       );
     } else {
-      Todos = todos.map(todo => (
+      const { sortedList } = this.state;
+      const sortedTodos = sortedList.length <= 0 ? todos : sortedList;
+
+      Todos = sortedTodos.map(todo => (
         <TodoItem
           todo={todo}
           key={todo._id}
           editing={todo._id === editingTodo}
           onEditingChange={this.onEditingChange}
+          onMoveItem={this.onMoveItem}
         />
       ));
     }
 
     return (
       <div className="page lists-show">
-        <ListHeader list={list} />
+        <ListHeader
+          list={list}
+          todosCount={todos.length}
+        />
         <div className="content-scrollable list-items">
           {loading
             ? <Message title={i18n.__('pages.listPage.loading')} />
